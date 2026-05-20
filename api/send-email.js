57fca4ddfa878a494138ec5ec9fbcff451aea777
almujtaba <almujtabashawgi@gmail.com>
2026-05-20 09:50:34 +0200

    pure test

--- api/send-email.js
+++ api/send-email.js
@@ -1,19 +1,3 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-export default async function handler(req, res) {
-
-  try {
-
-    return res.status(200).json({
-      keyExists: !!process.env.RESEND_API_KEY
-    });
-
-  } catch (error) {
-
-    return res.status(500).json({
-      error: error.message
-    });
-  }
+export default function handler(req, res) {
+  return res.status(200).send("WORKING");
 }
