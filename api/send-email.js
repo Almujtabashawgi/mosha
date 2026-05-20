faf07ae35535589342f061256d633d7bd8c76cbe
almujtaba <almujtabashawgi@gmail.com>
2026-05-20 09:47:59 +0200

    test send email route

--- api/send-email.js
+++ api/send-email.js
@@ -1,10 +1,13 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export default async function handler(req, res) {
 
   try {
 
     return res.status(200).json({
-      success: true,
-      body: req.body || null
+      keyExists: !!process.env.RESEND_API_KEY
     });
 
   } catch (error) {
