4fb2cc6a1e7a4cb4f1afa795dc54cbe7f111d2e8
almujtaba <almujtabashawgi@gmail.com>
2026-05-20 09:44:31 +0200

    fix js types

--- api/send-email.js
+++ api/send-email.js
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function handler(req: any, res: any) {
+export default async function handler(req, res) {
 
   if (req.method !== "POST") {
     return res.status(405).json({
